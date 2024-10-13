6705531420350a99d630aa469af47add95c6a09b
Jacob DeLong <62762261+jmdelong7@users.noreply.github.com>
2024-10-13 12:52:32 -0700

    Styling, basic layout, controller
    Styling and Basic Layout.
      - Prev, next arrows centered.
      - Circles for navigation below frame.
    
    Controller.
      - Add image creates new image element.
      - Add images to loop through array of images to add.
      - Adds circle and ability to click on circle to display image.

--- src/controller.js
+++ src/controller.js
@@ -2,13 +2,13 @@ import Carousel from "./carousel";
 import createImage from "./create-image";
 
 class Controller {
-  constructor(doc, carousel, frame, nextBtn, prevBtn, idxBtns) {
+  constructor(doc, carousel, frame, nextBtn, prevBtn, nav) {
     this.doc = doc;
     this.carousel = carousel;
     this.frame = frame;
     this.nextBtn = nextBtn;
     this.prevBtn = prevBtn;
-    this.idxBtns = idxBtns;
+    this.nav = nav;
   }
 
   nextPrevEventListeners() {
@@ -21,14 +21,34 @@ class Controller {
     });
   }
 
+  navEventListener(circle, img) {
+    circle.addEventListener('click', () => {
+      this.displayImg(img);
+    });
+  }
+
+  createNavCircle() {
+    const circle = this.doc.createElement('li');
+    circle.classList.add('circle');
+    this.nav.insertAdjacentElement('beforeend', circle);
+  }
+
   addImg(src, alt) {
     const img = createImage(this.doc, src, alt);
     this.carousel.addImg(img);
+    const circle = this.createNavCircle();
+    this.navEventListener(circle, img);
+  }
+
+  addImgs(...imgs) {
+    imgs.forEach((img) => {
+      this.addImg(img.src, img.alt);
+    });
   }
 
   displayImg(img) {
     this.frame.innerHTML = '';
-    this.frame.insertAdjacentHTML('beforeend', img);
+    this.frame.insertAdjacentElement('beforeend', img);
   }
 
   next() {
